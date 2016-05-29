398e777d76226e7e60fbaa0ba8ded89808296c9b
romagolchin <gol4in.roman@yandex.ru>
2016-05-29 23:39:58 +0330

    added reversi (not complete)

---
+++ src/reversi/ComputerPlayer.java
@@ -0,0 +1,10 @@
+package reversi;
+
+/**
+ * Created by Алексей on 5/29/2016.
+ */
+public class ComputerPlayer implements Player {
+    public void makeMove(BoardConfiguration configuration) throws MoveException {
+
+    }
+}
